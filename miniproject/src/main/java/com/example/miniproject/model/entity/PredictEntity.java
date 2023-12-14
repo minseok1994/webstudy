e48bfca763bb7d5d0073a01f390c8a33b1b7618f
minseok1994 <bmsshow94@gmail.com>
2023-12-14 14:44:12 +0900

    이그노어수정

--- miniproject/src/main/java/com/example/miniproject/model/entity/PredictEntity.java
+++ miniproject/src/main/java/com/example/miniproject/model/entity/PredictEntity.java
@@ -1,13 +1,24 @@
 package com.example.miniproject.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-@Entity(name = "StocksEntity")
-@Table(name = "stocks")
+@Entity(name = "PredictEntity")
+@Table(name = "predict")
 public class PredictEntity {
-    private Long id
-    private 
+    private Long id;
+    private String name;
+    private Long today;
+    private Long oneweek;
+    private Long twoweek;
 }

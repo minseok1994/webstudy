31a4c2d430465e380be73314c12a33d20ab19518
minseok1994 <bmsshow94@gmail.com>
2023-12-19 14:42:04 +0900

    Co-authored-by: mandupi <mandupi@users.noreply.github.com>

--- miniproject/src/main/java/com/example/miniproject/database/repository/PredictRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.miniproject.database.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.miniproject.model.entity.PredictEntity;
-
-public interface PredictRepository extends JpaRepository<PredictEntity, Long> {
-
-}
--- miniproject/src/main/java/com/example/miniproject/database/repository/StocksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.miniproject.database.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.miniproject.model.entity.StocksEntity;
-
-public interface StocksRepository extends JpaRepository<StocksEntity, Long> {
-
-}
